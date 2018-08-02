4e8ede12ec9bf0e298ad27d5550d8d8e150009ab
Risto Uuk <ristouuk@gmail.com>
2018-08-02 11:17:04 +0300

    Started using stringr options for cleaning data instead.

--- test.R
+++ test.R
@@ -3,15 +3,20 @@ ufc_data <- read_csv("data/ufc data - Sheet1.csv")
 View(ufc_data)
 
 library(tidyverse)
-ufc_data$method_d[grepl("Rear-Naked Choke", ufc_data$method_d)] <- "Rear-Naked Choke"
-ufc_data$method_d[grepl("Rear Naked Choke", ufc_data$method_d)] <- "Rear-Naked Choke"
-ufc_data$method_d[grepl("Guillotine Choke", ufc_data$method_d)] <- "Guillotine Choke"
-ufc_data$method_d[grepl("Armbar", ufc_data$method_d)] <- "Armbar"
-ufc_data$method_d[grepl("Triangle Choke", ufc_data$method_d)] <- "Triangle Choke"
-ufc_data$method_d[grepl("Kimura", ufc_data$method_d)] <- "Kimura"
-ufc_data$method_d[grepl("Heel Hook", ufc_data$method_d)] <- "Heel Hook"
-ufc_data$method_d[grepl("Brabo Choke", ufc_data$method_d)] <- "Brabo Choke"
-ufc_data_cleaned_chokes <- ufc_data
+  
+ufc_data$method_d <- str_replace(ufc_data$method_d, "Elbow$", "Elbows")
+ufc_data$method_d <- str_replace(ufc_data$method_d, "Rear Naked Choke", "Rear-Naked Choke")
+ufc_data$method_d <- str_replace(ufc_data$method_d, "Submission \\(Rear-Naked Choke", "Rear-Naked Choke")
+ufc_data$method_d <- str_replace(ufc_data$method_d, "Submission \\(Guillotine Choke", "Guillotine Choke")
+ufc_data$method_d <- str_replace(ufc_data$method_d, "Submission \\(Armbar", "Armbar")
+ufc_data$method_d <- str_replace(ufc_data$method_d, "Submission \\(Triangle Choke", "Triangle Choke")
+ufc_data$method_d <- str_replace(ufc_data$method_d, "Submission \\(Kimura", "Kimura")
+ufc_data$method_d <- str_replace(ufc_data$method_d, "Submission \\(Brabo Choke", "Brabo Choke")
+
+
+str_extract(ufc_data$method_d, "Elbow$")
+
+
 
 saveRDS(ufc_data_cleaned_chokes, here::here("data", "ufc_data_cleaned_chokes.rds"))
 
