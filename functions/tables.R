1dfe706492a754a9be42a39b0a4d57d772afc4eb
Risto Uuk <ristouuk@gmail.com>
2018-08-03 11:08:28 +0300

    Some changes to table functions

--- functions/tables.R
+++ functions/tables.R
@@ -1,9 +1,18 @@
 #---- Functions for tables ----
 
-table_top_5_wins <- function(data_ufc_method_clean = NULL) {
-  table <- data_ufc_method_clean %>% 
-    count(f1name) %>% 
-    filter(n > 16) 
+table_top_5_wins <- function(data_top_5_most_wins = NULL) {
+  table <- data_top_5_most_wins %>% 
+    count(f1name, method) %>% 
+    spread(method, n, fill = as.integer(0)) %>% 
+    adorn_totals(where = "col") %>% 
+    arrange(-Total) 
   
-  kable(table,  align  =  "c")   
+names(table) <- c("Fighter", "Decisions", "Knockouts", "Other", 
+                                 "Submissions", "Total") 
+
+table <- as.data.frame(table)
+  
+  kable(table,  align  =  "c")  %>%
+    kable_styling(full_width  =  TRUE) 
 }  
+
